f31da24a74b779ed5e3c9a91e0aae85dc2ebb153
BilliNovik <dangercreo2@aol.com>
2022-08-09 21:42:43 +0300

    change input search and margin

--- components/Widgets.tsx
+++ components/Widgets.tsx
@@ -7,11 +7,11 @@ const Widgets = ({ articles }: any) => {
     const [aricleNumber, setArticleNumber] = React.useState(5)
 
     return (
-        <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-5">
+        <div className="xl:w-[600px] hidden lg:inline ml-8 space-y-3">
             <div className="w-[90%] xl:w-[75%] sticky top-0 bg-white py-1.5 z-50">
                 <div className="flex items-center p-3 rounded-full bg-red-300 relative">
                     <SearchIcon className="h-5 z-50 text-gray-500" />
-                    <input className="transition duration-400 absolute inset-0 rounded-full pl-11 border-gray-500 text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100 " type="text" placeholder="Search Twitter" />
+                    <input className="transition duration-400 absolute inset-0 rounded-full pl-11 border-none text-gray-700 focus:shadow-lg focus:bg-white bg-gray-100 " type="text" placeholder="Search Twitter" />
                 </div>
             </div>
             <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2 w-[90%] overflow-hidden xl:w-[75%]">
