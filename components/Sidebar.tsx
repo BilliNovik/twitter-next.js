94d2459ce14b018c51c60485665d46b34c1ce45e
BilliNovik <dangercreo2@aol.com>
2022-08-10 21:23:17 +0300

    add logic ( sign in, sing out) and session

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,39 +1,54 @@
 import Image from 'next/image'
 import React from 'react'
-
-import SidebarMenuItem from './SidebarMenuItem'
-
-import logo from '../public/img/twitter-logo.png'
+import { useSession, signIn } from "next-auth/react"
 import { BellIcon, HomeIcon, InboxIcon } from '@heroicons/react/solid'
 import { DotsCircleHorizontalIcon, DotsHorizontalIcon, HashtagIcon, UserIcon } from '@heroicons/react/outline'
+
+import SidebarMenuItem from './SidebarMenuItem'
+import logo from '../public/img/twitter-logo.png'
 
 type Props = {
 
 }
 
 const Sidebar = (props: Props) => {
+
+    const { data: session }: any = useSession()
+
     return (
         <div className='sm-flex flex flex-col p-2 xl:items-start h-full fixed xl:mr-[370px]'>
             <div className='hoverEffect flex items-center justify-center hover:bg-blue-100 xl:justify-start '>
                 <Image src={logo} width="25" height="25" />
             </div>
-            <div className='pb-2 mb-2.5'>
+            <div className='pt-4 pb-2 mb-2.5'>
                 <SidebarMenuItem text="Home" icon={HomeIcon} active />
                 <SidebarMenuItem text="Explore" icon={HashtagIcon} />
-                <SidebarMenuItem text="Notifications" icon={BellIcon} />
-                <SidebarMenuItem text="Message" icon={InboxIcon} />
-                <SidebarMenuItem text="Profile" icon={UserIcon} />
-                <SidebarMenuItem text="More" icon={DotsCircleHorizontalIcon} />
+                {session &&
+                    <>
+                        <SidebarMenuItem text="Notifications" icon={BellIcon} />
+                        <SidebarMenuItem text="Message" icon={InboxIcon} />
+                        <SidebarMenuItem text="Profile" icon={UserIcon} />
+                        <SidebarMenuItem text="More" icon={DotsCircleHorizontalIcon} />
+                    </>
+                }
+
             </div>
-            <button className='bg-blue-400 mb-2 text-white rounded-full w-56 h-12 font-bold shadow-md hover:brightness-95 text-lg hidden xl:inline'>Tweet</button>
-            <div className='hoverEffect text-gray-700 flex items-center justify-start xl:justify-start mt-auto w-full'>
-                <Image src='http://tricky-photoshop.com/wp-content/uploads/2017/08/final-1.png' width='50' height='50' className='rounded-full' />
-                <div className='leading-5 hidden xl:inline xl:ml-2'>
-                    <h4 className='font-bold'>BNB Chain</h4>
-                    <span className='text-gray-500'>@BNBCHAIN</span>
+            {
+                session ? <button className='bg-blue-400 mb-2 text-white rounded-full w-56 h-12 font-bold shadow-md
+                    hover:brightness-95 text-lg hidden xl:inline'>Tweet</button>
+                    : <button className='bg-blue-400 mb-2 text-white rounded-full w-56 h-12 font-bold shadow-md 
+                    hover:brightness-95 text-lg hidden xl:inline' onClick={signIn}>Sign in</button>
+            }
+            {session &&
+                <div className='hoverEffect text-gray-700 flex items-center justify-start xl:justify-start mt-auto w-full'>
+                    <Image src={session.user.image} width='50' height='50' className='rounded-full' />
+                    <div className='leading-5 hidden xl:inline xl:ml-2'>
+                        <h4 className='font-bold'>{session.user.name}</h4>
+                        <span className='text-gray-500'>@{session.user.username}</span>
+                    </div>
+                    <DotsHorizontalIcon className='h-5 xl:ml-auto hidden xl:inline' />
                 </div>
-                <DotsHorizontalIcon className='h-5 xl:ml-auto hidden xl:inline' />
-            </div>
+            }
         </div>
     )
 }
